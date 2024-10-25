0133c6a5633f8eb348b3646455679302bba63c2b
anwarhossain1 <irishkhan33@gmail.com>
2024-10-25 15:57:44 +0600

    profile and book-now page added

---
+++ pages/profile.js
@@ -0,0 +1,14 @@
+import Layout from '@/components/layout/Layout';
+import PageTitle from '@/components/sections/PageTitle';
+
+export default function PageYachtSell() {
+  return (
+    <>
+      <Layout headerStyle={1} footerStyle={1}>
+        <PageTitle pageName="Profile" />
+        SOon
+        {/* <YachtSell /> */}
+      </Layout>
+    </>
+  );
+}
